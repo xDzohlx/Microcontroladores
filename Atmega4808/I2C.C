73ef3c47052791974e589cb918364f28cdb18737
Carlos Vega <69025860+xDzohlx@users.noreply.github.com>
2020-10-29 12:06:36 -0600

    Update I2C.C

--- Atmega4808/I2C.C
+++ Atmega4808/I2C.C
@@ -8,28 +8,33 @@
 #include <avr/io.h>
 #define USART0_BAUD_RATE(BAUD_RATE) ((float)(F_CPU * 64 / (16 * (float)BAUD_RATE)) + 0.5)
 #include <util/delay.h>
-#define TWI0_BAUD_RATE(BAUD_RATE) ((float)(F_CPU  / (2 * (float)BAUD_RATE)) + 10 + F_CPU)
+#define TWI0_BAUD_RATE(BAUD_RATE) ((float)(F_CPU  / (2 * (float)BAUD_RATE)) + 10 )
 
-void send(uint8_t dato);
+void send(void);
 
-void send(uint8_t dato){
-
+void send(void){
+	TWI0.MADDR = 0x27;
+	while(!(TWI0.MSTATUS & TWI_RXACK_bm))
+	TWI0.MDATA = 0b10101010;
 }
 
 
 void setup(void){
 	PORTA.DIRSET = PIN3_bm|PIN2_bm;
-	TWI0.MBAUD = (uint16_t)TWI0_BAUD_RATE(100000);
-	TWI0.MCTRLB |= TWI_FLUSH_bm;
+	TWI0.MBAUD = (uint16_t)TWI0_BAUD_RATE(100000);//Generacion de baud para TWI el valor debe ser de como 100
+	//TWI0.MBAUD = 0x64;
+	
 	TWI0.CTRLA |= TWI_SMEN_bm|TWI_ENABLE_bm;
-	TWI0.MSTATUS |= TWI_BUSSTATE0_bp;
+	TWI0.MSTATUS |= TWI_BUSSTATE0_bp;//buss idle
 	//TWI0.SADDRMASK = (0x27<<TWI_ADDRMASK0_bp);
-	TWI0.MADDR = 0x27;
-	TWI0.MDATA = 0b10101010;
+	TWI0.MCTRLB |= TWI_FLUSH_bm;
+	//TWI0.MADDR = 0x27;
+	//TWI0.MDATA = 0b10101010;
 }
 int main(void){
 	setup();
     while (1){
-
+		send();
+		_delay_ms(10);
     }
 }
