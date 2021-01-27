fa382f67022ae8260d6e41c56f5b9fee8333edc3
Carlos Vega <69025860+xDzohlx@users.noreply.github.com>
2021-01-26 22:20:48 -0600

    Update ADC828.c

--- Attiny828/ADC828.c
+++ Attiny828/ADC828.c
@@ -10,17 +10,17 @@
 
 int main(void)
 {
-		DDRC |= (1<<PORTC6);
-		ADCSRB |= (1<<ADLAR);//left adjust
-		ADMUXA |= (1<<MUX0);// adc canal 7
-		ADCSRA |= (1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADATE);//Frecuencia de muestreo 125kH:autotriger (1<<ADIE)|
+		DDRC |= (1<<PORTC6);//Pin de salida para un led
+		ADCSRB |= (1<<ADLAR);//Ajustado a la izquierda para una resolución de 8 bits
+		ADMUXA |= (1<<MUX0);// adc canal 1
+		ADCSRA |= (1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADATE);//Frecuencia de muestreo 125kH y lectura automatica
 		ADCSRA |= (1<<ADSC);//comienza el muestreo
 		
     while (1) {
-    if(ADCH>189){
-			PORTC &= ~(1<<PORTC6);
+    if(ADCH>189){//valor de 3.7 volts
+			PORTC &= ~(1<<PORTC6); //prende el led con un valor bajo
 	}else{
-		PORTC |= (1<<PORTC6);
+		PORTC |= (1<<PORTC6);	//prende el led con un valor alto
 	}
     }
 }
