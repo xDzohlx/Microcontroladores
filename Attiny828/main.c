cfd0a6de5fedb9d7162552394c45738fc406c0aa
Carlos Vega <69025860+xDzohlx@users.noreply.github.com>
2021-01-26 22:17:53 -0600

    Add files via upload
    Configuración del ADC sensillo para prender un led con una lectura de aproximadamente 3.6 Volts para un detector de baja bateria de una celda de lipo con alimentacion de 5 volts

---
+++ Attiny828/main.c
@@ -0,0 +1,27 @@
+/*
+ * Bateria.c
+ *
+ * Created: 25/01/2021 20:57:45
+ * Author : Usuario
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+		DDRC |= (1<<PORTC6);
+		ADCSRB |= (1<<ADLAR);//left adjust
+		ADMUXA |= (1<<MUX0);// adc canal 7
+		ADCSRA |= (1<<ADEN)|(1<<ADIE)|(1<<ADPS2)|(1<<ADPS1)|(1<<ADATE);//Frecuencia de muestreo 125kH:autotriger (1<<ADIE)|
+		ADCSRA |= (1<<ADSC);//comienza el muestreo
+		
+    while (1) {
+    if(ADCH>189){
+			PORTC &= ~(1<<PORTC6);
+	}else{
+		PORTC |= (1<<PORTC6);
+	}
+    }
+}
+
